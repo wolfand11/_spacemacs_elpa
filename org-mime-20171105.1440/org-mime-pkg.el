83bed696eefd4cf53d3962dce814fd83be4a651e
wolfand11 <smile_guodong@163.com>
2018-01-28 13:14:40 +0800

    update spacemacs

---
+++ org-mime-20171105.1440/org-mime-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-mime" "20171105.1440" "org html export for text/html MIME emails" '((emacs "24.3") (cl-lib "0.5")) :commit "64a3f56651369b2a8b0a601abea1cf1216db2ded" :url "http://github.com/org-mime/org-mime" :keywords '("mime" "mail" "email" "html"))
--- winum-20170217.110/winum-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "winum" "20170217.110" "Navigate windows and frames using numbers." '((cl-lib "0.5")) :url "http://github.com/deb0ch/winum.el" :keywords '("convenience" "frames" "windows" "multi-screen"))
