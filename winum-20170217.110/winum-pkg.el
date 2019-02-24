1fcce26eb8e8617b4b03c6ad290b9ea667d44637
guodong <smile_guodong@163.com>
2019-02-24 22:20:15 +0800

    add org-page plugin

---
+++ winum-20170217.110/winum-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "winum" "20170217.110" "Navigate windows and frames using numbers." '((cl-lib "0.5")) :url "http://github.com/deb0ch/winum.el" :keywords '("convenience" "frames" "windows" "multi-screen"))
