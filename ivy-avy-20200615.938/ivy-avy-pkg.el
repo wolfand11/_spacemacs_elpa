dd379d7afd1fe390edaaf8aea2c113f676c63961
wolfand11 <smile_guodong@163.com>
2020-10-20 16:46:16 +0800

    update packages

---
+++ ivy-avy-20200615.938/ivy-avy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ivy-avy" "20200615.938" "Avy integration for Ivy" '((emacs "24.5") (ivy "0.13.0") (avy "0.5.0")) :commit "b65e401c22ec56a008b00f651cd9536caf593d43" :keywords '("convenience") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
